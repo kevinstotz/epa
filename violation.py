99df34d8d209b11e13ff453775d70d90f2d58687
kevinstotz email = kevin_stotz@yahoo.com <ubuntu@ip-172-31-14-148.us-west-2.compute.internal>
2017-08-25 12:35:47 +0000

    added separate tables

--- violation.py
+++ violation.py
@@ -10,8 +10,9 @@ from datetime import datetime
 from datetime import timedelta
 
 TABLES = ("VIOLATION_ENF_ASSOC","LCR_SAMPLE","TREATMENT","LCR_SAMPLE_RESULT","SERVICE_AREA","GEOGRAPHIC_AREA","ENFORCEMENT_ACTION","VIOLATION","WATER_SYSTEM_FACILITY","WATER_SYSTEM")
-STATUS_TABLE="status"
 MAX_ATTEMPTS=5
+table=''
+STATUS_TABLE=''
 UPDATE_TIME_DELTA=10
 SLEEP_TIME=5
 TABLE=''
@@ -39,55 +40,66 @@ def let_user_pick(tables):
 
     if i == 1:
         table="VIOLATION_ENF_ASSOC"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID','2': 'VIOLATION_ID','3': 'ENFORCEMENT_ID'}
         fields = {'1': 'PWSID','2': 'VIOLATION_ID','3': 'ENFORCEMENT_ID'}
 
     if i == 2:
         table="LCR_SAMPLE"
+        STATUS_TABLE="status." + table
         primary_keys  = {'1': 'PWSID', '2': 'SAMPLE_ID'}
         fields = {'1': 'PWSID','2': 'SAMPLE_ID','3': 'SAMPLING_END_DATE','4':'SAMPLING_START_DATE','5':'RECONCILIATION_ID','6':'PRIMACY_AGENCY_CODE','7':'EPA_REGION'}
 
     if i == 3:
         table="TREATMENT"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID', '2': 'TREATMENT_ID','3': 'FACILITY_ID'}
         fields = {'1': 'PWSID','2': 'TREATMENT_ID','3': 'FACILITY_ID','4':'TREATMENT_OBJECTIVE_CODE','5':'TREATMENT_PROCESS_CODE','6':'COMMENTS_TEXT'}
 
     if i == 4:
         table = "LCR_SAMPLE_RESULT"
+        STATUS_TABLE="status." + table
+        STATUS_TABLE=table
         primary_keys = {'1': 'PWSID', '2': 'SAMPLE_ID'}
         fields = {'1': 'PWSID','2': 'SAMPLE_ID','3': 'EPA_REGION','4':'PRIMACY_AGENCY_CODE','5':'CONTAMINANT_CODE','6':'RESULT_SIGN_CODE','7':'SAMPLE_MEASURE','8':'SAR_ID','9':'UNIT_OF_MEASURE'}
 
     if i == 5:
         table = "SERVICE_AREA"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID'} 
         fields = {'1': 'PWSID','2': 'EPA_REGION','3': 'IS_PRIMARY_SERVICE_AREA_CODE','4':'PRIMACY_AGENCY_CODE','5':'PWS_ACTIVITY_CODE','6':'PWS_TYPE_CODE', '7':'SERVICE_AREA_TYPE_CODE'}
 
     if i == 6:
         table = "GEOGRAPHIC_AREA"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID','2': 'GEO_ID'} 
         fields = {'1': 'PWSID','2': 'GEO_ID','3': 'ANSI_ENTITY_CODE','4':'AREA_TYPE_CODE','5':'CITY_SERVED','6':'COUNTY_SERVED', '7':'EPA_REGION','8': 'PRIMACY_AGENCY_CODE','9': 'PWS_ACTIVITY_CODE','10': 'PWS_TYPE_CODE','11':'STATE_SERVED','12':'TRIBAL_CODE','13':'ZIP_CODE_SERVED'}
 
     if i == 7:
         table = "ENFORCEMENT_ACTION"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID', '2': 'ENFORCEMENT_ID'}
         fields = {'1': 'PWSID','2': 'ENFORCEMENT_ID','3': 'ORIGINATOR_CODE','4':'ENFORCEMENT_DATE','5':'ENFORCEMENT_ACTION_TYPE_CODE','6':'ENFORCEMENT_COMMENT_TEXT'}
 
     if i == 8:
         table = "VIOLATION"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID', '2': 'VIOLATION_ID'}
         fields = {'1':  'PWSID', '2':  'VIOLATION_ID', '3':  'COMPL_PER_END_DATE', '4':  'CONTAMINANT_CODE', '5':  'CORRECTIVE_ACTION_ID', '6':  'EPA_REGION', '7':  'FACILITY_ID', '8':  'IS_HEALTH_BASED_IND', '9':  'IS_MAJOR_VIOL_IND', '10':  'LATEST_ENFORCEMENT_ID', '11':  'ORIGINATOR_CODE', '12':  'POPULATION_SERVED_COUNT', '13':  'POP_CAT_5_CODE', '14':  'PRIMACY_AGENCY_CODE', '15':  'PRIMARY_SOURCE_CODE', '16':  'PUBLIC_NOTIFICATION_TIER', '17':  'COMPLIANCE_STATUS_CODE', '18':  'PWS_ACTIVITY_CODE', '19':  'PWS_DEACTIVATION_DATE', '20':  'PWS_TYPE_CODE', '21':  'RTC_DATE', '22':  'RTC_ENFORCEMENT_ID', '23':  'RULE_CODE', '24':  'RULE_FAMILY_CODE', '25':  'RULE_GROUP_CODE', '26':  'SAMPLE_RESULT_ID', '27':  'SEVERITY_IND_CNT', '28':  'STATE_MCL', '29':  'UNIT_OF_MEASURE', '30':  'VIOLATION_CATEGORY_CODE', '31':  'VIOLATION_CODE', '32':  'COMPL_PER_BEGIN_DATE', '33':  'VIOL_MEASURE'}
 
     if i == 9:
         table = "WATER_SYSTEM_FACILITY"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID', '2': 'FACILITY_ID'}
         fields = {'1': 'PWSID', '2': 'FACILITY_ID', '3': 'FACILITY_ACTIVITY_CODE', '4': 'FACILITY_DEACTIVATION_DATE', '5': 'EPA_REGION', '6': 'FACILITY_NAME', '7': 'FACILITY_TYPE_CODE', '8': 'FILTRATION_STATUS_CODE', '9': 'IS_SOURCE_IND', '10': 'IS_SOURCE_TREATED_IND', '11': 'PRIMACY_AGENCY_CODE', '12': 'AVAILABILITY_CODE', '13': 'PWS_ACTIVITY_CODE', '14': 'PWS_DEACTIVATION_DATE', '15': 'PWS_TYPE_CODE', '16': 'SELLER_PWSID', '17': 'SELLER_PWS_NAME', '18': 'SELLER_TREATMENT_CODE', '19': 'STATE_FACILITY_ID', '20': 'SUBMISSION_STATUS_CODE', '21': 'WATER_TYPE_CODE'}
 
     if i == 10:
         table = "WATER_SYSTEM"
+        STATUS_TABLE="status." + table
         primary_keys = {'1': 'PWSID'}
         fields = {'1': 'PWSID', '2': 'ADDRESS_LINE1', '3': 'ADDRESS_LINE2', '4': 'ADMIN_NAME', '5': 'ALT_PHONE_NUMBER', '6': 'CDS_ID', '7': 'CITY_NAME', '8': 'COUNTRY_CODE', '9': 'DBPR_SCHEDULE_CAT_CODE', '10': 'EMAIL_ADDR', '11': 'EPA_REGION', '12': 'FAX_NUMBER', '13': 'GW_SW_CODE', '14': 'IS_GRANT_ELIGIBLE_IND', '15': 'IS_SCHOOL_OR_DAYCARE_IND', '16': 'IS_WHOLESALER_IND', '17': 'LT2_SCHEDULE_CAT_CODE', '18': 'ORG_NAME', '19': 'OUTSTANDING_PERFORMER', '20': 'OUTSTANDING_PERFORM_BEGIN_DATE', '21': 'OWNER_TYPE_CODE', '22': 'PHONE_EXT_NUMBER', '23': 'PHONE_NUMBER', '24': 'POPULATION_SERVED_COUNT', '25': 'POP_CAT_11_CODE', '26': 'POP_CAT_2_CODE', '27': 'POP_CAT_3_CODE', '28': 'POP_CAT_4_CODE', '29': 'POP_CAT_5_CODE', '30': 'PRIMACY_AGENCY_CODE', '31': 'PRIMACY_TYPE', '32': 'PRIMARY_SOURCE_CODE', '33': 'PWS_ACTIVITY_CODE', '34': 'PWS_DEACTIVATION_DATE', '35': 'PWS_NAME', '36': 'PWS_TYPE_CODE', '37': 'SEASON_BEGIN_DATE', '38': 'SEASON_END_DATE', '39': 'SERVICE_CONNECTIONS_COUNT', '40': 'SOURCE_PROTECTION_BEGIN_DATE', '41': 'SOURCE_WATER_PROTECTION_CODE', '42': 'STATE_CODE', '43': 'SUBMISSION_STATUS_CODE', '44': 'ZIP_CODE'}
 
-    return (table, primary_keys, fields)
+    return (STATUS_TABLE, table, primary_keys, fields)
 
 
 def exists_in_db(id):
@@ -127,6 +139,7 @@ def insert_record(data, url):
          cleaned = re.sub(r'[^\u0000-\uFFFF]+', '', str(data[value]), flags=re.IGNORECASE)
          query = query + value + '="' + cleaned + '", '
      query = 'INSERT INTO ' + TABLE + ' SET ' + query + ' url="' + str(url) + '"'
+     print(query)
      insert_into_db(query)
 
 
@@ -177,9 +190,12 @@ def update_record(data, url):
      return mysql.custom_query(query)  
 
 def get_stats():
-        records_remaining = mysql.custom_query('SELECT count(id) FROM ' + STATUS_TABLE + '  WHERE ' + TABLE + '=0')
-        records_completed = mysql.custom_query('SELECT count(id) FROM ' + STATUS_TABLE + '  WHERE ' + TABLE + '=1')
-        records_total = mysql.custom_query('SELECT count(id) FROM ' + STATUS_TABLE)
+        sql = 'SELECT count(id) FROM ' + STATUS_TABLE + '  WHERE ' + TABLE + '=0'
+        records_remaining = mysql.custom_query(sql)
+        sql = 'SELECT count(id) FROM ' + STATUS_TABLE + '  WHERE ' + TABLE + '=1'
+        records_completed = mysql.custom_query(sql)
+        sql = 'SELECT count(id) FROM ' + STATUS_TABLE
+        records_total = mysql.custom_query(sql)
 
         return records_total,records_completed,records_remaining
 
@@ -193,6 +209,7 @@ def get_next():
 
 def update_status_in_db(pwsid, code):
    sql = 'UPDATE ' + STATUS_TABLE + ' set ' + TABLE + '=' + str(code) + ' WHERE ' + FIELDS['1'] + '="'+pwsid+'" limit 1'
+   print(sql)
    res = mysql.custom_query(sql)
    return res
 
@@ -221,10 +238,10 @@ def get_contents(url):
                update_status_in_db(pwsid, 2)
                return 0, "" 
           sleep(SLEEP_TIME)
-        return 0, "" 
+        return 10, "" 
 
 
-(TABLE, PRIMARY_KEYS, FIELDS) = let_user_pick(TABLES)
+(STATUS_TABLE, TABLE, PRIMARY_KEYS, FIELDS) = let_user_pick(TABLES)
 
 id=1
 while (pwsid != 0):
@@ -249,8 +266,10 @@ while (pwsid != 0):
         #else:
         url = 'https://iaspub.epa.gov/enviro/efservice/'+TABLE+'/PWSID/' + pwsid + '/JSON/'
         results, contents = get_contents(url)
+        print("results=",results)
         if results > 0:
             parse_json(str(contents), url)
             update_status_in_db(pwsid, 1)
-
+        if results == 10:
+            update_status_in_db(pwsid, 10)
 exit()
